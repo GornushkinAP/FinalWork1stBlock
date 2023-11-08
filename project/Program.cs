7caa455b1983e566b97c4acc81ba3fd1dbc70998
GornushkinAP <gapedu@yandex.ru>
2023-11-09 01:09:32 +0300

    написал программу пишу ридми

--- project/Program.cs
+++ project/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string[] inputArray = InputData();
+
+       
+        string[] filteredArray = FilterData(inputArray);
+
+        
+        OutputData(filteredArray);
+    }
+
+    
+    static string[] InputData()
+    {
+        Console.WriteLine("Введите элементы массив строк (через запятую и пробел):");
+        string input = Console.ReadLine();
+
+        
+        string[] inputArray = input.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        return inputArray;
+    }
+
+  
+    static string[] FilterData(string[] inputArray)
+    {
+        
+        int count = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        
+        string[] filteredArray = new string[count];
+        int index = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                filteredArray[index] = str;
+                index++;
+            }
+        }
+
+        return filteredArray;
+    }
+
+    
+    static void OutputData(string[] filteredArray)
+    {
+        Console.WriteLine("Отфильтрованный массив:");
+        if (filteredArray.Length == 0)
+        {
+            Console.WriteLine("Пусто");
+        }
+        else
+        {
+            foreach (string str in filteredArray)
+            {
+                Console.Write(str);
+            }
+        }
+    }
+}
